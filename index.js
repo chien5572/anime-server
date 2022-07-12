84e79a8706720a068c3783cccb419298df857f41
nguyenthanhchien102 <nguyenthanhchien102@gmail.com>
2022-07-13 00:14:03 +0700

    get homepage

--- index.js
+++ index.js
@@ -1,22 +1,93 @@
 const cheerio = require('cheerio');
 const request = require('request-promise');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-request('https://animehay.club/', (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html); 
+function crawlListAnime($) {
+    let listAnime = [];
+    $('.movie-item').each((index, el) => {
+        const movieItem = $(el).find('a');
+        const id = $(el).attr('id').slice(9);
+        const name = movieItem.attr('title');
+        const link = movieItem.attr('href');
+        const episodeNumber = movieItem.find('.episode-latest span').text();
+        const image = movieItem.find('div img').attr('data-cfsrc');
+        const alt = movieItem.find('div img').attr('alt');
+        // const anime = new Anime(id, name, link, episodeNumber, image, alt);
+        listAnime.push({
+            id, name, link, episodeNumber, image, alt
+        });
+    })
+    return listAnime;
+}
 
-        $('.movie-item').each((index, el) => { 
-            const anime = $(el).find('a');
-            // const id = $(el).attr('id').;
-            const name = anime.attr('title');
-            const link = anime.attr('href');
-            const episodeNumber = anime.find('.episode-latest span').text();
-            const image = anime.find('div img').attr('data-cfsrc');
-            const alt = anime.find('div img').attr('alt');
-
-            console.log(image);
-        })
-    } else {
-        console.log(error);
+function getSlide(n, mode) {
+    const list = []
+    switch (mode) {
+        case 1:
+            for (let i of n) {
+                list.push(i['children']['0']['data']);
+            }
+            break;
+        case 2:
+            for (let i of n) {
+                list.push(i['children']['0']['data'].slice(1));
+            }
+            break;
+        case 3:
+            for (let i of n) {
+                list.push(i['attribs']['href'])
+            }
+            break;
+        case 4:
+            for (let i of n) {
+                list.push(i['attribs']['data-cfsrc'])//['data-cfsrc'])
+            }
+            break;
     }
-});
+
+    return list;
+}
+
+function crawlSlide($) {
+    let listSlice = [];
+    $('.ah-carousel').each((index, el) => {
+        const slide = $(el).find('div');
+        const name = getSlide(slide.find('.name'), 1)
+        const episode = getSlide(slide.find('.episode_latest'), 2);
+        const link = getSlide(slide.find('a'), 3);
+        const img = getSlide(slide.find('img'), 4)
+        for (let i in name) {
+            listSlice.push({
+                'name': name[i],
+                'episode': episode[i],
+                'link': link[i],
+                'img': img[i]
+            })
+        }
+        // console.log(listSlice);
+    })
+    return listSlice;
+}
+
+
+app.get('/', (req, res) => {
+    request('https://animehay.club/', (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            const listAnime = crawlListAnime($);
+            const listSlice = crawlSlide($);
+            res.send({
+                'slide': listSlice,
+                'animes': listAnime
+            })
+        } else {
+            console.log(error);
+        }
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
